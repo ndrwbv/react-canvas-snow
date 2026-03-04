951d1e7aa03e7b2d4ff42986bd7b1d0b6ab3cb12
ndrwbv <ndrwbv@gmail.com>
2026-03-04 13:23:32 +0700

    change path

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
-import path from 'node:path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   base: process.env.VITE_BASE ?? '/',
-  build: {
-    outDir: path.resolve(__dirname, 'demo-dist'),
-  },
 });
