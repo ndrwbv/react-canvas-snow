d48ee311bc65e203f568845ba1bf57c3e78941f2
ndrwbv <ndrwbv@gmail.com>
2026-03-04 13:12:40 +0700

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from 'node:path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -5,6 +6,6 @@ export default defineConfig({
   plugins: [react()],
   base: process.env.VITE_BASE ?? '/',
   build: {
-    outDir: 'demo-dist',
+    outDir: path.resolve(__dirname, 'demo-dist'),
   },
 });
